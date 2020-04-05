119e96bb710e5634b842bc9665e42bc6e795c459
lcaratin <leocaratin@gmail.com>
2020-04-05 03:17:54 -0300

    Adding Transaction Routes (getTransactions/getTransactionsByProduct)

--- controllers/api/transactions.controller.js
+++ controllers/api/transactions.controller.js
@@ -4,22 +4,78 @@ var router = express.Router();
 var transactionService = require('services/transactions.service');
 var productService = require('services/products.service');
 
-router.post('/register', registerTransaction); 
+router.get('/', getTransactions);
+router.get('/:_id', getTransactionsByProduct);
+router.post('/register', registerTransaction); //
+// router.get('/:_id', getById); //
+// router.put('/:_id', updateProduct); //
+// router.delete('/:_id', deleteProduct); //
 
 module.exports = router;
 
+function getTransactions(req, res) {
+    transactionService.getAll()
+        .then(function (transacts) {
+            res.send(transacts);
+        })
+        .catch(function (err) {
+            res.status(400).send(err);
+        });
+}
+
+function getTransactionsByProduct(req, res) {
+    transactionService.getAllByProduct(req.params._id)
+        .then(function (transacts) {
+            res.send(transacts);
+        })
+        .catch(function (err) {
+            res.status(400).send(err);
+        });
+}
 
 function registerTransaction(req, res) {
     transactionService.create(req.body)
         .then(function () {
-            console.log('first then');
             productService.updateStockQuantity(req.body);
         })
         .then(function(){
-            console.log('second then');
             res.sendStatus(200);
         })
         .catch(function (err) {
             res.status(400).send(err);
         });
 }
+
+// function getById(req, res) {
+//     transactionService.getById(req.params._id)
+//         .then(function (user) {
+//             if (user) {
+//                 res.send(user);
+//             } else {
+//                 res.sendStatus(404);
+//             }
+//         })
+//         .catch(function (err) {
+//             res.status(400).send(err);
+//         });
+// }
+
+// function updateProduct(req, res) {
+//     transactionService.update(req.params._id, req.body)
+//         .then(function () {
+//             res.sendStatus(200);
+//         })
+//         .catch(function (err) {
+//             res.status(400).send(err);
+//         });
+// }
+
+// function deleteProduct(req, res) {
+//     transactionService.delete(req.params._id)
+//         .then(function () {
+//             res.sendStatus(200);
+//         })
+//         .catch(function (err) {
+//             res.status(400).send(err);
+//         });
+// }
--- services/transactions.service.js
+++ services/transactions.service.js
@@ -11,11 +11,38 @@ var productService = require('services/products.service');
 
 var service = {};
 
+service.getAll = getAll;
+service.getAllByProduct = getAllByProduct;
 service.create = create;
+// service.getById = getById;
+// service.update = update;
+// service.delete = _delete;
 
 module.exports = service;
 
+function getAll() {
+    var deferred = Q.defer();
 
+    db.transactions.find({}).toArray(function (err, questions) {
+        if (err) deferred.reject(err.name + ': ' + err.message);
+
+        deferred.resolve(questions);
+    });
+
+    return deferred.promise;
+}
+
+function getAllByProduct(_id) {
+    var deferred = Q.defer();
+
+    db.transactions.find({productId: _id}).toArray(function (err, transacts) {
+        if (err) deferred.reject(err.name + ': ' + err.message);
+
+        deferred.resolve(transacts);
+    });
+
+    return deferred.promise;
+}
 
 function create(transactParam) {
     var deferred = Q.defer();
@@ -32,3 +59,99 @@ function create(transactParam) {
 
     return deferred.promise;
 }
+
+// function getById(_id) {
+//     var deferred = Q.defer();
+
+//     db.transactions.findById(_id, function (err, transact) {
+//         if (err) deferred.reject(err.name + ': ' + err.message);
+
+//         if (transact) {
+//             deferred.resolve(transact);
+//         } else {
+//             deferred.resolve();
+//         }
+//     });
+
+//     return deferred.promise;
+// }
+
+// function update(_id, transactParam) {
+//     var deferred = Q.defer();
+
+//     db.transactions.findById(_id, function (err, product) {
+//         if (err) deferred.reject(err.name + ': ' + err.message);
+//         else updateTransact();
+//     });
+
+//     function updateTransact() {
+//         var set = {
+//             quantity: transactParam.quantity,
+//             transact: transactParam.transact
+//         };
+
+//         db.transactions.update(
+//             { _id: mongo.helper.toObjectID(_id) },
+//             { $set: set },
+//             function (err, doc) {
+//                 if (err) deferred.reject(err.name + ': ' + err.message);
+
+//                 //deferred.resolve();
+//                 productService.updateStockQuantity(transactParam, deferred);
+//             }
+//         );
+//     }
+
+//     function updateProduct() {
+//         // fields to update
+
+//         db.transactions.findById(_id, function (err, transact) {
+//             if (err) deferred.reject(err.name + ': ' + err.message);
+    
+//             if (transact) {
+//                 deferred.resolve(transact);
+//             } else {
+//                 deferred.resolve();
+//             }
+//         });
+
+//         var set = {
+//             name: transactParam.name,
+//             type: transactParam.type,
+//             brand: transactParam.brand,
+//             characteristic: transactParam.characteristic,
+//             color: transactParam.color,
+//             size: transactParam.size,
+//             purchaseTagPrice: transactParam.purchaseTagPrice,
+//             purchasePrice: transactParam.purchasePrice,
+//             hundredPercentPrice: transactParam.hundredPercentPrice,
+//             suggestedPrice: transactParam.suggestedPrice
+//         };
+
+//         db.transactions.update(
+//             { _id: mongo.helper.toObjectID(_id) },
+//             { $set: set },
+//             function (err, doc) {
+//                 if (err) deferred.reject(err.name + ': ' + err.message);
+
+//                 deferred.resolve();
+//             }
+//         );
+//     }
+
+//     return deferred.promise;
+// }
+
+// function _delete(_id) {
+//     var deferred = Q.defer();
+
+//     db.transactions.remove(
+//         { _id: mongo.helper.toObjectID(_id) },
+//         function (err) {
+//             if (err) deferred.reject(err.name + ': ' + err.message);
+
+//             deferred.resolve();
+//         });
+
+//     return deferred.promise;
+// }
